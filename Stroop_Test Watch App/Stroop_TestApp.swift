f3835ca0a32169911becbf3bf785e6b0d8343ca0
Pedro Santos <pedrohenriqueph004@gmail.com>
2026-08-14 18:30:14 -0300

    Initial Commit

---
+++ Stroop_Test Watch App/Stroop_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stroop_TestApp.swift
+//  Stroop_Test Watch App
+//
+//  Created by Pedro Santos on 14/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Stroop_Test_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
