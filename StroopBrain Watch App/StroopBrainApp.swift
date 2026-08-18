1369ae3de9a7404382dd528d26833a752967bfb2
Pedro Santos <pedrohenriqueph004@gmail.com>
2026-08-17 23:05:14 -0300

    feat: rename project to StroopBrain

--- StroopBrain Watch App/StroopBrainApp.swift
+++ StroopBrain Watch App/StroopBrainApp.swift
@@ -1,15 +1,15 @@
 //
-//  Stroop_TestApp.swift
-//  Stroop_Test Watch App
+//  StroopBrainApp.swift
+//  StroopBrain Watch App
 //
-//  Created by Pedro Santos on 14/08/26.
+//  Created by Pedro Santos on 17/08/26.
 //
 
 import SwiftUI
 import SwiftData
 
 @main
-struct Stroop_Test_Watch_AppApp: App {
+struct StroopBrain_Watch_AppApp: App {
     var body: some Scene {
         WindowGroup {
             StartGame()
---
+++ StroopBrain Watch AppTests/StroopBrain_Watch_AppTests.swift
@@ -0,0 +1,19 @@
+//
+//  StroopBrain_Watch_AppTests.swift
+//  StroopBrain Watch AppTests
+//
+//  Created by Pedro Santos on 17/08/26.
+//
+
+import Testing
+@testable import StroopBrain_Watch_App
+
+struct StroopBrain_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
